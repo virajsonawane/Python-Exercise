e7935d6a95c695d2af852e89a33589c9a640714d
Viraj Sonawane <virajsonawane22@gmail.com>
2026-01-19 09:41:23 +0530

    19-01-2026

---
+++ Assignment 10/Question4.py
@@ -0,0 +1,12 @@
+def main():
+    print("Enter Number : ")
+    num = int(input())
+
+    for i in range(1, num + 1):
+        
+        if i % 2 == 0:
+
+            print(i)
+
+if __name__ == "__main__":
+    main()
