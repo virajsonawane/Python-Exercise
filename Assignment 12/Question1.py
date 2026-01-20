4fc16e2f9eff8f4eb81bd00ad0239daf6a7a8b20
Viraj Sonawane <virajsonawane22@gmail.com>
2026-01-21 00:36:57 +0530

    Assignment 12 Q1, Q2

---
+++ Assignment 12/Question1.py
@@ -0,0 +1,11 @@
+def main():
+    print("Enter a character : ")
+    ch = input()
+    if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U':
+        print("Vowel")
+    
+    else:
+        print("Consonant")
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment 12/Question2.py
@@ -0,0 +1,11 @@
+def main():
+
+    print("Enter a Number : ")
+    num = int(input())
+    
+    for i in range(1, num + 1):
+        if num % i == 0:
+            print(i)
+
+if __name__ == "__main__":
+    main()
