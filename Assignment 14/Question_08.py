12fa6d6c0c78962963105fce5d6fe96135d90f41
Viraj Sonawane <virajsonawane22@gmail.com>
2026-01-24 00:58:49 +0530

    Assignment 14 Q8 & Q9

---
+++ Assignment 14/Question_08.py
@@ -0,0 +1,11 @@
+Addition = lambda n1, n2: n1 + n2
+
+def main():
+
+    num1 = int(input("Enter First Number : "))
+    num2 = int(input("Enter Second Number : "))
+
+    print(Addition(num1, num2))
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment 14/Question_09.py
@@ -0,0 +1,11 @@
+Multiplication = lambda n1, n2: n1 * n2
+
+def main():
+
+    num1 = int(input("Enter First Number : "))
+    num2 = int(input("Enter Second Number : "))
+
+    print(Multiplication(num1, num2))
+
+if __name__ == "__main__":  
+    main()
