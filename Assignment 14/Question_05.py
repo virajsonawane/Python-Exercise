e82d2ef8677f3053d144b2bc7697f5f0612063ce
Viraj Sonawane <virajsonawane22@gmail.com>
2026-01-24 00:45:53 +0530

    Assignment 14 Q5 with correction

--- Assignment 14/Question_05.py
+++ Assignment 14/Question_05.py
@@ -1,10 +1,10 @@
-Even = lambda n: "Even" if (n % 2 == 0) else "Odd"
+Even = lambda n: "True" if (n % 2 == 0) else "False"
 
 def main():
 
     num = int(input("Enter a Number : "))
 
-    print("The number is >>>>", Even(num))
+    print( Even(num))
 
 if __name__ == "__main__":
     main()
